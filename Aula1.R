1a06300bf1c31769e89b4e2d0ef696379a5e1a39
bbiasi <brennerbiasiss@gmail.com>
2019-08-15 13:56:46 -0300

    Aula 1

---
+++ Aula1.R
@@ -0,0 +1,85 @@
+
+# Brenner
+# Aula 1 - R
+
+
+# use # no inicio da frase p comentarios
+# evite usar acentos, pontuacao, etc
+
+# 04 tracos (----) indicam paginacao
+
+
+# Meu diretorio de trabalho ----
+setwd("D:/PEI/ENGD02/2019_2/ENGD02")
+
+# Criando objeto ----
+
+
+Maria <- AA
+#Error: object 'AA' not found
+
+Maria <- "AA"
+
+Jose <- "11"
+Jose <- 111
+
+Area <- 10
+
+x <- Jose - Area
+x <- Jose * Area
+
+vetor <- c(1.80, 1.69, 1.71, 1.59, 2.01, 1.67, 1.82, 1.63)
+vetor2 <- c("AA", "BB", 1.71, 1.59, 2.01, 1.67, 1.82, 1.63)
+
+rm(vetor2)
+# remove(vetor2)
+
+print(vetor)
+View(vetor)
+
+vetor_2 <- vetor + 1
+
+df <- data.frame("Coluna 1" = vetor,
+                 "XYZ" = vetor_2)
+
+print(df)
+
+
+# Estatisticas basicas----
+media <- mean(vetor)
+mediana <- median(vetor)
+desvio <- sd(vetor)
+var <- var(vetor)
+
+var
+media; mediana
+
+paste0("A média é: ", media)
+
+
+# Importando banco de dados----
+
+library(readxl)
+dados <- read_excel("C:/Users/Lucas Mascarenhas/Desktop/Rascunhos R/Aula - Matematica Instrumental/EconomistData.xlsx", 
+                    col_types = c("text", "numeric", "numeric", 
+                                  "numeric", "text"))
+
+# Graficos----
+
+plot(x = dados$CPI, y = dados$HDI, xlab = "CPI", ylab = "HDI")
+hist(x = dados$CPI)
+boxplot(x = dados$CPI)
+
+# Exportando dados----
+
+tabela <- data.frame(desvio, media, mediana, var)
+write.csv2(x = tabela, file = "tabela_saida.csv")
+
+# Extra----
+
+## Para acessar uma variavel dentro de um banco de dados...
+## ...utilizamos '$' apos o banco de dados de interesse
+
+media.CPI <- mean(dados$CPI)
+
+
